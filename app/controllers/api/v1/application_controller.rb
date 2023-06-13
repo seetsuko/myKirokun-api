c613e649c3b47795c48ffc12bc7273f4ec936210
seetsuko <radgjms@gmail.com>
2023-06-13 15:00:14 +0900

    refactor: APIをテストしたいので、一度リクエストヘッダをコメントアウトした

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,6 +1,10 @@
 module Api
   class V1::ApplicationController < ActionController::API
-    include Firebase::Auth::Authenticable
-    before_action :authenticate_user
+    # V1::ApplicationControllerを継承したコントローラのメソッドにアクセス時、HTTPリクエストヘッダに、「Authorization: “Bearer *****************(トークン)”」を入れるだけで、勝手にJWT認証してくれるようになります。
+
+    # APIテストのため一旦コメントアウト
+    
+    # include Firebase::Auth::Authenticable
+    # before_action :authenticate_user
   end
 end
