a06686cad8b16b9dade942fab25f4a345cfd4f57
seetsuko <radgjms@gmail.com>
2023-06-13 15:02:16 +0900

    feat: リクエストヘッダがindexのみになってしまっていたので、全体に使うためコードを修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   namespace 'api' do
     namespace 'v1' do
-      resources :time_logs, only: %i(index)
+      resources :time_logs
       namespace 'auth' do
         post 'registrations' => 'registrations#create'
       end
