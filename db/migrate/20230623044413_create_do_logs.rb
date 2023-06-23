f3656e4b0b3c24746924f4ecb146d67911067b06
seetsuko <radgjms@gmail.com>
2023-06-23 14:02:08 +0900

    fix: DoLogのテーブルが使えなかったので作り直した。

--- db/migrate/20230623044413_create_do_logs.rb
+++ db/migrate/20230623044413_create_do_logs.rb
@@ -1,6 +1,6 @@
 class CreateDoLogs < ActiveRecord::Migration[7.0]
   def change
-    create_table :do_logs , id: :string do |t|
+    create_table :do_logs, id: :string do |t|
       t.string :time
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_23_042701) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_24_171123) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "do_lists", id: :string, force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "do_logs", id: :string, force: :cascade do |t|
+    t.string "time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
