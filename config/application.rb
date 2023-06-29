44a21f380daefe147fba853dacac56d5249098c9
seetsuko <radgjms@gmail.com>
2023-06-29 13:02:09 +0900

    fix: redis接続エラーのため修正

--- config/application.rb
+++ config/application.rb
@@ -2,13 +2,14 @@ require_relative "boot"
 
 require "rails/all"
 require 'sprockets/railtie'
+require_relative 'initializers/redis'
 
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module NondaApi3
+module MyKirokun_Api
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
---
+++ config/initializers/redis.rb
@@ -0,0 +1,6 @@
+require 'redis'
+
+$redis = Redis.new(
+  host:'red-ciefkktgkuvlk1nik58g',
+  port:6379
+)
