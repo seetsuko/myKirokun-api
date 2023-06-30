fbcd34ec9370e099203a621d6e8f9e169284d9e4
seetsuko <radgjms@gmail.com>
2023-06-30 11:54:09 +0900

    fix: フロント側がデプロイしたので、corsのurlを変更した

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
+    origins "https://my-kirokun-nr2rio184-seetsuko.vercel.app'"
 
     resource "*",
       headers: :any,
